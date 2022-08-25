1970e17260c89f293ff7a3a1bbeab399747dd433
Gabriel T Marques <gabrieltmm@gmail.com>
2022-08-25 01:27:59 -0300

    added some navigation to the application and changed migration to have correct associations between Users and Balances

---
+++ db/migrate/20220825030404_create_balances.rb
@@ -0,0 +1,11 @@
+class CreateBalances < ActiveRecord::Migration[7.0]
+  def change
+    create_table :balances do |t|
+      t.float      :amount
+
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_25_004056) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_25_030404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "balances", force: :cascade do |t|
+    t.float "amount"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_balances_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", default: ""
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -24,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_25_004056) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
