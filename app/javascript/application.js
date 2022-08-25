e4163dab862b080c1184b64c5b3e121aa2665d3e
Gabriel T Marques <gabrieltmm@gmail.com>
2022-08-24 23:01:19 -0300

    devise sign_out endpoint now is done via GET requests, and added a timeout to clear 'notice' and 'alert' paragraphs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const notice = document.getElementById('notice-paragraph')
+  const alert = document.getElementById('alert-paragraph')
+
+  let noticeTimeout, alertTimeout;
+
+  if (notice) {
+    noticeTimeout = setTimeout(function() {
+      notice.remove();
+      clearTimeout(noticeTimeout);
+    }, 3000);
+  }
+
+  if (alert) {
+    alertTimeout = setTimeout(function() {
+      alert.remove();
+      clearTimeout(alertTimeout);
+    }, 3000);
+  }
+})
